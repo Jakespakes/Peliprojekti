4ae8cf9f64af2e5da99f0abf03189217ff7d64eb
JukkaPekka <jakespakes@protonmail.com>
2026-09-03 17:32:39 +0300

    Python prototyyppi

---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+import time
+import numpy as np
+
+def start():
+    x = True
+    while x == True:
+        answer = input("Aloitetaanko? (Y/n) ")
+
+        if answer == "y" or answer == "Y":
+            print("Aloitetaan!")
+            x = False
+        elif answer == "n" or answer == "N":
+            print("Kysytään uudestaan. ")
+        else: 
+            print("Laita (Y/n). ")
+
+def game():
+    play_again = True
+    score = 0
+    increment = 1.5
+    sub = 0.3
+
+    while play_again:
+        colors = {}
+
+        # Määritellään num1-4 arvot
+        for i in range(1, 5):
+            colors[f"num{i}"] = random.randint(1, 4)
+
+        # Tulostetaan taulun arvot
+        for key, value in colors.items():
+            print(f"{value}")
+            time.sleep(increment)
+
+        # Itse arvailulogiikka + kokonaistuloksen laskeminen
+        for i in range(1, 5):
+            try:
+                guess = int(input(f"{i}. luku? "))
+            except:
+                print("Anna kokonaisluku apina \n")
+                continue
+
+            if guess == colors[f"num{i}"]:
+                print("OIKEIN")
+                score += 1
+            else: 
+                print("VÄÄRIN")
+                play_again = False
+                print("Kokonaistulos: ", score)
+
+        increment = max(0, increment - sub)
+        
+#start()
+game()
