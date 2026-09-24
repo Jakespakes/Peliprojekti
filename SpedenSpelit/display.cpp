f7a33697e9ffb5acddcf7a206f8e37275ef5c5a9
JukkaPekka <jakespakes@protonmail.com>
2026-09-24 20:56:12 +0300

    bugikorjaus näytöille

--- SpedenSpelit/display.cpp
+++ SpedenSpelit/display.cpp
@@ -62,8 +62,8 @@ void writeByte(uint8_t bits,bool last)
 
 void writeHighAndLowNumber(uint8_t tens,uint8_t ones)
 {
-  writeByte(numberBits[tens], false);
-  writeByte(numberBits[ones], true);
+  writeByte(numberBits[ones], false);
+  writeByte(numberBits[tens], true); 
 }
 
 void showResult(byte number)
